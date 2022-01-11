5b3ca02d893e8740b02feb171cebc1b4c77094d3
Luis <luisandrade195@gmail.com>
2022-01-11 01:22:39 -0300

    casi todo listo

--- desafioManejoArchivos/manejoArchivos.js
+++ desafioManejoArchivos/manejoArchivos.js
@@ -4,26 +4,83 @@ class Contenedor {
     constructor(nombreArchivo) {
         this.nombreArchivo = nombreArchivo;
     }
+    save(producto){
+        fs.promises.readFile('productos.txt', 'utf8')
+            .then(contenido => {
 
-    save(object){
+                let objetoEditable = JSON.parse(contenido)
+                producto.id = objetoEditable.length + 1
+                objetoEditable.push(producto)
+                let objetoAlmacenar = JSON.stringify(objetoEditable)
 
-        let objetos = fs.readFileSync('productos.txt','utf-8').toString()
-        let objetos2 = JSON.parse(objetos)
-        let idNueva = objetos2.length + 1
+                fs.promises.writeFile('productos.txt', objetoAlmacenar)
+                    .then((p) => {
+                        console.log('Objeto guardado y su ID es: ', producto.id)
+                    })
+                    .catch(() => {
+                        console.log('error de guaraddo')
+                    })
+            })
+        .catch(err => {
+            console.log('error de lectura', err)
+        })
 
-        object.id = idNueva
-
-
-        fs.appendFile('productos.txt',object, err => {
-                if(err){
-                    console.log('No se pudo guardar')
+    }
+    getById(id){
+        fs.promises.readFile('productos.txt','utf-8')
+            .then(contenido => {
+                let objetoEditable = JSON.parse(contenido)
+                if (id >= 0 && id < objetoEditable.length){
+                    console.log(objetoEditable[id-1])
                 }
                 else {
-
-                    console.log('guardado')
+                    console.log(null)
                 }
+
             })
-        }
+            .catch(err => {
+                console.log('error de lectura', err)
+            })
     }
-    
-}
+    getAll(){
+        fs.promises.readFile('productos.txt','utf-8')
+            .then(contenido => {
+                let objetoEditable = JSON.parse(contenido)
+                console.log(objetoEditable)
+            })
+            .catch(err => {
+                console.log('error de lectura', err)
+            })
+
+    }
+    deleteById(id){
+        fs.promises.readFile('productos.txt','utf-8')
+            .then(contenido => {
+                let objetoEditable = JSON.parse(contenido)
+                if (id >= 0 && id < objetoEditable.length){
+                    objetoEditable = objetoEditable.splice(id-1)
+                    let objetoAlmacenar = JSON.stringify(objetoEditable)
+
+                    fs.promises.writeFile('productos.txt', objetoAlmacenar)
+                        .then(() => {
+                            console.log('Objeto guardado ')
+                        })
+                        .catch(() => {
+                            console.log('error de guaraddo')
+                        })
+                }
+                else {
+                    console.log(null)
+                }
+
+
+
+            })
+            .catch(err => {
+                console.log('error de lectura', err)
+            })
+
+    }
+
+    }
+
--- desafioManejoArchivos/pruebas.js
+++ desafioManejoArchivos/pruebas.js
@@ -1,7 +1,120 @@
 const fs = require('fs')
+//agregar
+async function hola(texto) {
+    try {
+        await fs.promises.appendFile('productos.txt', texto)
+        console.log('agregado')
 
-const a = fs.readFileSync('productos.txt','utf-8').toString()
-const a2 = JSON.parse(a)
+    }
+    catch (err){
+        console.log('hubo un error')
+    }
+
+}
+//abrir y obtener lo que esta en el archivo
+
+async function escribir(texto){
+    try{
+        await fs.promises.writeFile('productos.txt', texto)
+        console.log('guardado')
+    }
+    catch (err){
+        console.log('no pudo ser guardado')
+    }
+}
+
+function leerTC(producto){
+    let id
+
+    fs.promises.readFile('productos.txt', 'utf8')
+        .then(contenido => {
+
+            objetoEditable = JSON.parse(contenido)
+            producto.id = objetoEditable.length + 1
+            id = producto.id
+            objetoEditable.push(producto)
+            //console.log(objetoEditable)
+            objetoAlmacenar = JSON.stringify(objetoEditable)
+            //console.log(objetoAlmacenar)
+
+            fs.promises.writeFile('productos.txt', objetoAlmacenar)
+                .then(prod=>{
+                    id = producto.id
+                })
+                .catch(err => {
+                        console.log('error de guaraddo', err)
+                })
+            return id
+        })
+        .catch(err => {
+            console.log('error de lectura', err)
+        })
+    console.log(id)
+}
+
+function getById(id){
+    fs.promises.readFile('productos.txt','utf-8')
+        .then(contenido => {
+            let objetoEditable = JSON.parse(contenido)
+            if (id >= 0 && id < objetoEditable.length){
+                console.log(objetoEditable[id-1])
+            }
+            else {
+                console.log(null)
+            }
+
+        })
+        .catch(err => {
+            console.log('error de lectura', err)
+        })
+}
+
+function deleteById(id){
+    fs.promises.readFile('productos.txt','utf-8')
+        .then(contenido => {
+            let objetoEditable = JSON.parse(contenido)
+            if (id >= 0 && id < objetoEditable.length){
+                objetoEditable = objetoEditable.splice(1, id-1)
+                let objetoAlmacenar = JSON.stringify(objetoEditable)
+
+                fs.promises.writeFile('productos.txt', objetoAlmacenar)
+                    .then(() => {
+                        console.log('Objeto guardado ')
+                    })
+                    .catch(() => {
+                        console.log('error de guaraddo')
+                    })
+            }
+            else {
+                console.log(null)
+            }
 
 
-console.log(a2)
+
+        })
+        .catch(err => {
+            console.log('error de lectura', err)
+        })
+
+}
+let p = [
+    {
+        nombre: 'luis',
+        id: 3
+    },
+    {
+        nombre: 'juan',
+        id: 7
+    }
+]
+
+
+
+
+
+
+
+
+
+
+
