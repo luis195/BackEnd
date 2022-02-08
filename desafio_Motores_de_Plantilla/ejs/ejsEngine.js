9cd34a238254650df20b51ef4f90fbd2487a6b70
Luis Andrade <luisandrade195@gmail.com>
2022-02-08 16:36:36 -0300

    motores de plantilla listo y acopmodado un bug en ejs

--- desafio_Motores_de_Plantilla/ejs/ejsEngine.js
+++ desafio_Motores_de_Plantilla/ejs/ejsEngine.js
@@ -2,47 +2,19 @@ const express =require('express');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
-let listaProductos = [
-    {
-        "title": "lapiz",
-        "price": 13,
-        "thumbnail": "https://cdn1.iconfinder.com/data/icons/material-design-icons-light/24/pencil-256.png",
-        "id": 1
-    },
-    {
-        "title": "regla",
-        "price": 10,
-        "thumbnail": "https://cdn0.iconfinder.com/data/icons/fitness-95/24/height-measurement-256.png",
-        "id": 2
-    },
-    {
-        "title": "cuaderno",
-        "price": 40,
-        "thumbnail": "https://cdn1.iconfinder.com/data/icons/office-322/24/notebook-book-notepad-spiral-128.png",
-        "id": 3
-    },
-    {
-        "title": "auriculares",
-        "price": 20,
-        "thumbnail": "https://cdn2.iconfinder.com/data/icons/lightly-icons/30/headphones-480.png",
-        "id": 4
-    },
-    {
-        "title": "resma de papel",
-        "price": 77,
-        "thumbnail": "https://cdn1.iconfinder.com/data/icons/document-edit-line/64/Document-doc-file-paper-report-bundle-sheaf-512.png",
-        "id": 5
-    }
-]
-listaProductos = [{}]
+const producto = require('./controller/api.js')
+let listaProductos = new producto.ProductosApi()
+console.log(listaProductos.listarAll())
 const ejs = require ("ejs")
 const {load} = require("nodemon/lib/rules");
 app.set('view engine', ejs);
-console.log(listaProductos.title)
+
+
 
 app.get('/productos',(req,res) =>{
+
     res.render('pages/form.ejs', {
-        lista: listaProductos
+        lista: listaProductos.listarAll()
     })
 })
 
@@ -53,7 +25,8 @@ app.post('/productos',(req, res) => {
     nuevoProducto.price = req.body.price
     nuevoProducto.thumbnail = req.body.thumbnail
     nuevoProducto.id = listaProductos.length + 1
-    listaProductos.push(nuevoProducto)
+    listaProductos.guardar(nuevoProducto)
+    console.log()
     res.redirect('/productos')
 
 })
